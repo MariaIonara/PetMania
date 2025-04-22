497e18c5fa60989c91296a61920ccbab9ee92b99
Ionara porto <ionara.porto@academico.ifpb.edu.br>
2025-04-22 09:35:13 -0300

    Texto que representa as mudanças realizadas

--- src/lib/usuario.js
+++ src/lib/usuario.js
@@ -1,6 +1,6 @@
 import db from './db';
 
-export asnyc function getUsuarios() {
+export async function getUsuarios() {
         const result = await db.query('SELECT * FROM')
         return result.rows;
 }
