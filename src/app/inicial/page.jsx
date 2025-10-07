fe4f5121820bb3e185d4f5dbf2000211698b12c2
Ionara porto <ionara.porto@academico.ifpb.edu.br>
2025-10-07 11:51:19 -0300

    Tela de login

--- src/app/inicial/page.jsx
+++ src/app/inicial/page.jsx
@@ -4,11 +4,11 @@ import BackgroundDividido from "../components/fundo.js"
 export default function Page() {
     return (
         <BackgroundDividido>
-
-            <h1 className={styles.titulo}>Pet Mania</h1>
-
-            <div className={styles.caixaCentralizadaInicio}>
-                <button className={styles.botao} type="submit">Iniciar</button>
+            <div className={styles.inicio}>
+                <h1 className={styles.titulo}>Pet Mania</h1>
+                <div className={styles.caixaCentralizadaInicio}>
+                    <button className={styles.botao} type="submit">Iniciar</button>
+                </div>
             </div>
         </BackgroundDividido>
     );
--- src/app/loginUsuario/page.jsx
+++ src/app/loginUsuario/page.jsx
@@ -1,116 +1,31 @@
-'use client'
-
 import styles from "./page.module.css";
-import BackgroundDividido from "../components/Invertido.js"
-
-import { useEffect, useState } from "react";
+import BackgroundDividido from "../components/fundo.js"
+import Link from "next/link";
 
 
 export default function Page() {
-    /* const [nome, setNome] = useState();*/
-    /*const alunos = await db.query("select * from usuario")*/
-    const [email, setEmail] = useState("");
-    const [senha, setSenha] = useState("");
-
-    useEffect(() => {
-      const fetchRacas = async () => {
-        const response = await fetch('/api/raca')
-        const data = await response.json()
-        setRacas(data);
-      };
-      
-      fetchRacas();
-  }, []);
-
-
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-
-        const formData = new FormData();
-
-        formData.append('nomedopet', nomedopet);
-        formData.append('idadedopet', idadedopet);
-        formData.append('enderecocidade', enderecocidade);
-        formData.append('raca', raca);
-        formData.append('imagem', imagem);
-        formData.append('sexo', sexo);
-
-        try {
-            const res = await fetch('/api/cadastrarPet', {
-                method: 'POST',
-                body: formData,
-            });
-
-            if (res.ok) {
-                setMensagem('Pet cadastrado com sucesso!');
-                setNomeDoPet("");
-                setIdadeDoPet("");
-                setRaca("");
-                setEnderecocidade("");
-                setSexo("");
-                setImagem(null);
-            } else {
-                const err = await res.json();
-                setMensagem(`Erro: ${err.error || 'Não foi possível cadastrar.'}`);
-            }
-        } catch (error) {
-            console.error(error);
-            setMensagem("Erro ao enviar os dados.")
-        }
-    }
-
-
     return (
-
         <BackgroundDividido>
-            <div>
-        
-                <h1 className={styles.tituloCadastre}>Cadastre seu Pet</h1>
+            <div className={styles.caixaCentralizada}>
+              <h2 className={styles.tituloLogin}>LOGIN</h2>
+                <input className={styles.caixaDeTexto} type="text" placeholder="Nome" />
+                <input className={styles.caixaDeTexto} type="password" placeholder="Senha" />
+                 <Link href="">Esqueceu a senha?</Link> 
+                <button className={styles.botaoLogin}> Login </button>
+              <div className={styles.blocoRegistrar}>
+            <Link href="">Ainda não possui nenhuma conta?</Link>
+  
+</div>
 
-                <form onSubmit={handleSubmit}>
-
-                    <fieldset className={styles.caixaCentralizada}>
-                        <input type="file" onChange={(e) => setImagem(e.target.files[0])} accept="image/png image/jpg image/jpeg" />
-                    </fieldset>
-
-                    <fieldset className={styles.caixaCentralizada}>
-                        <input className={styles.caixaDeTexto} type="text" placeholder="Nome do Pet" value={nomedopet} onChange={(e) => setNomeDoPet(e.target.value)} />
-                    </fieldset>
-
-                    <fieldset className={styles.caixaCentralizada}>
-                        <input className={styles.caixaDeTexto} type="number" placeholder="Idade do Pet" value={idadedopet} onChange={(e) => setIdadeDoPet(e.target.value)} />
-                    </fieldset>
+               <button className={styles.botaoregistrar}> Registre-se </button>
+              <Link href="" className={styles.linkGoogle}> <img src="/google.png" alt="Google" className={styles.iconGoogle} />
+                 Continuar com o Google
+              </Link>
 
 
-                    <fieldset className={styles.caixaCentralizada}>
-                        <select className={styles.caixaDeTexto} name="Raça" value={raca} onChange={(e) => setRaca(e.target.value)}>
-                            <option value="Raça">Raça</option>
-                            {racas.map((item) => <option key={item.id} value={item.id}>{item.nome}</option> )}
-                        </select>
-                    </fieldset>
 
-                    <fieldset className={styles.caixaCentralizada}>
-                        <input className={styles.caixaDeTexto} type="text" placeholder="Endereço(cidade)" value={enderecocidade} onChange={(e) => setEnderecocidade(e.target.value)} required/>
-                    </fieldset>
-
-                    <fieldset className={styles.radioGroup}>
-                        <label className={styles.radioLabel}>
-                            <input  type="radio" name="sexo" value="femea" checked={sexo === "femea"} onChange={(e) => setSexo(e.target.value)} /> Fêmea
-                        </label>
-                        <br />
-                        <label className={styles.radioLabel}>
-                            <input type="radio" name="sexo" value="macho" checked={sexo === "macho"} onChange={(e) => setSexo(e.target.value)} /> Macho
-                        </label>
-                    </fieldset>
-
-                    <div className={styles.caixaCentralizada}>
-                        <button className={styles.botao} type="submit">Registrar</button>   
-                    </div>
-
-                    {mensagem && <p>{mensagem}</p>}
-                </form>
             </div>
+ 
         </BackgroundDividido>
-    );
-}
+    )
+};
