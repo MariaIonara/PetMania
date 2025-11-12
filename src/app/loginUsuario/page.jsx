fc08fac71a440fb0bb8b6e54488801fb95ff8738
Ionara Porto <ionara.porto@academico.ifpb.edu.br>
2025-11-12 08:13:57 -0300

    Erro na tela de loginUsuario

--- src/app/loginUsuario/page.jsx
+++ src/app/loginUsuario/page.jsx
@@ -1,54 +1,81 @@
-import styles from "./page.module.css";
-import BackgroundDividido from "../components/fundo.js"
+'use client';
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+//import { signIn, useSession } from "next-auth/react";
 import Link from "next/link";
-import { signIn, useSession } from "next-auth/react";
-
+import BackgroundDividido from "../components/fundo.js";
+import styles from "./page.module.css";
 
 export default function Page() {
+  //const { data: session } = useSession();
+  const router = useRouter();
 
-  const { data: session } = useSession();
-
-  if (session) {
-    router.replace("/telaPrincipal");
-    return null;
-  }
-
-  const handleLogin = async (e) => {
-    e.preventDefault()
-    try {
-      const res = await signIn("credentials", {
-        redirect: false,
-        email,
-        senha
-      });
-      if (res?.ok) route.push("/telaPrincipal");
-      else alert("Email e senha inválidos");
-    } catch (error) {
-      console.error(error)
-      alert('Erro de conexão')
-    }
-  }
-
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+  /*
+   if (session) {
+     router.replace("/telaPrincipal");
+     return null;
+   }
+  
+   const handleLogin = async (e) => {
+     e.preventDefault();
+     try {
+       const res = await signIn("credentials", {
+         redirect: false,
+         email,
+         senha,
+       });
+       if (res?.ok) router.push("/telaPrincipal");
+       else alert("Email e senha inválidos");
+     } catch (error) {
+       console.error(error);
+       alert("Erro de conexão");
+     }
+   };
+ */
   return (
     <BackgroundDividido>
       <div className={styles.caixaCentralizada}>
         <h2 className={styles.tituloLogin}>LOGIN</h2>
-        <input className={styles.caixaDeTexto} type="text" placeholder="Nome" />
-        <input className={styles.caixaDeTexto} type="password" placeholder="Senha" />
+
+        <input
+          className={styles.caixaDeTexto}
+          type="text"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          className={styles.caixaDeTexto}
+          type="password"
+          placeholder="Senha"
+          value={senha}
+          onChange={(e) => setSenha(e.target.value)}
+        />
+
         <Link href="">Esqueceu a senha?</Link>
-        <button className={styles.botaoLogin}> Login </button>
+
+        <button onClick={console.log("TESTE 2")} className={styles.botaoLogin}>
+          Login
+        </button>
+
         <div className={styles.blocoRegistrar}>
           <Link href="">Ainda não possui nenhuma conta?</Link>
-
         </div>
 
-        <button className={styles.botaoregistrar}> Registre-se </button>
-        <Link href="" className={styles.linkGoogle}> <img src="/google.png" alt="Google" onClick={() => signIn("google")}  className={styles.iconGoogle} />
+        <button className={styles.botaoregistrar}>Registre-se</button>
+
+        <Link href="" className={styles.linkGoogle}>
+          <img
+            src="/google.png"
+            alt="Google"
+            onClick={() => console.log("TESTE")}
+            className={styles.iconGoogle}
+          />
           Continuar com o Google
         </Link>
-
       </div>
-
     </BackgroundDividido>
-  )
-};
+  );
+}
